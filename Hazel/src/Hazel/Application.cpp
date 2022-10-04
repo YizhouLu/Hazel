e1535fa4a462821e4f076337dc957e5342e2f82c
Yizhoulu <luyizhou@umich.edu>
2022-10-03 23:30:44 -0400

    ImGui

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -3,14 +3,19 @@
 
 #include "Hazel/Log.h"
 
-#include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 namespace Hazel {
 
 #define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
 
+Application* Application::s_Instance = nullptr;
+
 Application::Application()
 {
+    HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
+    s_Instance = this;
+
     m_Window = std::unique_ptr<Window>(Window::Create());
     m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 }
@@ -22,11 +27,13 @@ Application::~Application()
 void Application::PushLayer(Layer* layer)
 {
     m_LayerStack.PushLayer(layer);
+    layer->OnAttach();
 }
 
 void Application::PushOverlay(Layer* overlay)
 {
     m_LayerStack.PushOverlay(overlay);
+    overlay->OnAttach();
 }
 
 void Application::OnEvent(Event& e) {
