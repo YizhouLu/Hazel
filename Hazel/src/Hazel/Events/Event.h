28afcb40ac48a621d9879083a37e0d7118e6764b
Yizhoulu <luyizhou@umich.edu>
2022-10-27 21:53:30 -0400

    misc fix

--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -50,17 +50,15 @@ public:
 };
 
 class EventDispatcher {
-template<typename T>
-using EventFcn = std::function<bool(T&)>;
-
 public:
 	EventDispatcher(Event& event)
 		: m_Event(event) {}
 
-	template<typename T>
-	bool Dispatch(EventFcn<T> fcn) {
+	// F will be deduced by the compiler
+	template<typename T, typename F>
+	bool Dispatch(const F& fcn) {
 		if (m_Event.GetEventType() == T::GetStaticType()) {
-			m_Event.Handled = fcn(*(T*)&m_Event);
+			m_Event.Handled = fcn(static_cast<T&>(m_Event));
 			return true;
 		}
 		return false;
